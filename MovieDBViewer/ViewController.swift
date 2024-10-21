e4f7795b76b65f05c33d6e034d842f4ba76f29c3
jakwan <dzakwan.yahya@binus.ac.id>
2024-10-21 11:36:47 +0700

    Initial Commit

---
+++ MovieDBViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieDBViewer
+//
+//  Created by admin on 21/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
---
+++ MovieDBViewerTests/MovieDBViewerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDBViewerTests.swift
+//  MovieDBViewerTests
+//
+//  Created by admin on 21/10/24.
+//
+
+import Testing
+@testable import MovieDBViewer
+
+struct MovieDBViewerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
